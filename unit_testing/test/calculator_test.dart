cb3f1a5190f1f0835d172dade28bf7503d7f94f6
Aleksey Kuzmin <kboomska@gmail.com>
2023-12-09 11:44:15 +0300

    calculator test (add); testing completed with an error

--- unit_testing/test/calculator_test.dart
+++ unit_testing/test/calculator_test.dart
@@ -5,6 +5,6 @@ import 'package:unit_testing/calculator.dart';
 void main() {
   test('The calculator return 5 when adding 1 and 4', () {
     final calculator = Calculator();
-    expect(calculator.add(1, 4), 5);
+    expect(calculator.add(1, 4), 6, reason: 'It should be exactly 5');
   });
 }
