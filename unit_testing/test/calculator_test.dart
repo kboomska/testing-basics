c700f5635a4e1993b36b91d5863a155baaa3eaca
Aleksey Kuzmin <kboomska@gmail.com>
2023-12-09 11:46:20 +0300

    calculator test (add); adding Arrange, Act, Assert structure

--- unit_testing/test/calculator_test.dart
+++ unit_testing/test/calculator_test.dart
@@ -4,7 +4,16 @@ import 'package:unit_testing/calculator.dart';
 
 void main() {
   test('The calculator return 5 when adding 1 and 4', () {
+    // Arrange (Setup) -> create the calculator object.
+    // Создается и настраивается тестируемый модуль.
     final calculator = Calculator();
-    expect(calculator.add(1, 4), 6, reason: 'It should be exactly 5');
+
+    // Act (Action) -> collect the result you want to test.
+    // Запускается модуль с некоторым состоянием.
+    final result = calculator.add(1, 4);
+
+    // Assert (Result) -> compare the result against and expected value.
+    // Убеждаемся, что модуль ведет себя так, как ожидалось.
+    expect(result, 5, reason: 'It should be exactly 5');
   });
 }
