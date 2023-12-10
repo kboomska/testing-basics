e4e75a4c92e6033b990ed1dc1667dab0bac29df2
Aleksey Kuzmin <kboomska@gmail.com>
2023-12-10 18:53:25 +0300

    calculator widget testing; OperationWidget testing only

--- widget_testing/test/calculator_app_test.dart
+++ widget_testing/test/calculator_app_test.dart
@@ -1,10 +1,18 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import 'package:widget_testing/calculator.dart';
 import 'package:widget_testing/main.dart';
+import 'package:widget_testing/operation.dart';
 import 'package:widget_testing/operation_widget.dart';
 
 void main() {
+  late Calculator calculator;
+
+  setUp(() {
+    calculator = Calculator();
+  });
+
   group('CalculatorApp', () {
     testWidgets('Render 4 widgets of Type OperationWidget', (widgetTester) async {
       // Create the widget - CalculatorApp()
@@ -33,6 +41,34 @@ void main() {
       // await widgetTester.pump(); // Trigger a frame.
 
       // Repeat pump() until there are no longer any frames scheduled.
+      await widgetTester.pumpAndSettle();
+
+      expect(textResultFinder, findsOneWidget);
+    });
+  });
+
+  group('Operation Widget', () {
+    testWidgets('Render 10 when 3 and 7 added', (widgetTester) async {
+      // OperationWidget testing only. But MaterialApp is required!
+      await widgetTester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: OperationWidget(
+              operation: Operation.add,
+              calculator: calculator,
+            ),
+          ),
+        ),
+      );
+
+      final topTextFieldFinder = find.byKey(const Key('textField_top_Addition'));
+      final bottomTextFieldFinder = find.byKey(const Key('textField_bottom_Addition'));
+
+      await widgetTester.enterText(topTextFieldFinder, '3');
+      await widgetTester.enterText(bottomTextFieldFinder, '7');
+
+      final textResultFinder = find.text('Result: 10.0');
+
       await widgetTester.pumpAndSettle();
 
       expect(textResultFinder, findsOneWidget);
