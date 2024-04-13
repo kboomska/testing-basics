2013be08128aa4e5facc892bd6a7c4ad16b2b03b
Aleksey Kuzmin <kboomska@gmail.com>
2024-04-13 20:04:37 +0300

    TDD; Default User test ("Write a failing test" stage)

---
+++ test_driven_development/test/user_test.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Default User test', () {
+    User user = User();
+
+    test('User has an id with type int', () {
+      expect(user.id, isA<int>());
+    });
+
+    test('User has a name with type String', () {
+      expect(user.name, isA<String>());
+    });
+
+    test('Default id = 0. Default name = "No name"', () {
+      expect(user.id, equals(0));
+      expect(user.name, equals('No name'));
+    });
+  });
+}
