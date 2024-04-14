350cefb9687bdf60434ec19a1b9b2586109ed878
Aleksey Kuzmin <kboomska@gmail.com>
2024-04-14 16:36:29 +0300

    TDD; Set up mock Dio

---
+++ test_driven_development/test/user_remote_repository_test.dart
@@ -0,0 +1,13 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockDio extends Mock implements Dio {}
+
+void main() {
+  late MockDio mockDio;
+
+  setUp(() {
+    mockDio = MockDio();
+  });
+}
